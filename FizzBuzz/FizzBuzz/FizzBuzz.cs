c642d480d046de5f6b1068692c88db148406c9e7
Kareem <kareem335@yahoo.co.uk>
2019-09-27 20:54:54 +0100

    fizzbuzz challenge completed with TDD

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,12 @@ namespace FizzBuzz
     {
         public static object FizzBuzz(int number)
         {
-            if (number % 3 == 0)
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            else if (number % 3 == 0)
             {
                 return "Fizz";
             }
--- FizzBuzz/FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzzTest/FizzBuzzTest.cs
@@ -23,5 +23,21 @@ namespace FizzBuzzTest
             Assert.AreEqual(FizzBuzzStarter.FizzBuzz(number), "Buzz");
         }
 
+        [TestCase]
+        public void When_Number_Is_Divisableby_Both_3_And_5_Returns_FizzBuzz()
+        {
+            int number = 15;
+
+            Assert.AreEqual(FizzBuzzStarter.FizzBuzz(number), "FizzBuzz");
+        }
+
+        [TestCase]
+        public void When_Number_Is_Not_Divisable_By_Either_Return_Number()
+        {
+            int number = 7;
+
+            Assert.AreEqual(FizzBuzzStarter.FizzBuzz(number), number);
+        }
+
     }
 }
