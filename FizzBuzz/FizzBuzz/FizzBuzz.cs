1a730429c58fb6e26946f0c83c4b50a491667226
Kareem <kareem335@yahoo.co.uk>
2019-09-27 20:12:30 +0100

    fizzbuzz now returns fizz when divisable by three

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzStarter
+    {
+        public static string FizzBuzz(int number)
+        {
+            if (number == 3)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return "Buzz";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,18 @@
+using FizzBuzz;
+using NUnit.Framework;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [TestCase]
+        public void When_Number_Is_Divisable_By_3_Returns_Fizz()
+        {    
+            int number = 3;
+
+            Assert.AreEqual(FizzBuzzStarter.FizzBuzz(number), "Fizz");
+        }
+    }
+}
